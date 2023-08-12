129653d0b3179290c0271ae36096d88fe5a6e9dc
zubair <imzubairkhan786@gmail.com>
2023-08-12 10:12:12 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { MantineProvider } from '@mantine/core';
+import App from './App';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MantineProvider withGlobalStyles withNormalizeCSS 
+      theme={{
+        globalStyles: (_theme) => ({
+          body:{
+            width:'100vw',
+            height:'100vh'
+          },
+          "#root":{
+            width:'100%',
+            height:'100%'
+          }
+        })
+      }}
+    >
+      <App />
+    </MantineProvider>
+  </React.StrictMode>,
+)
